957792949c7a42163e8bc2ffea1db02983533540
asrma7 <asrma.sharma@gmail.com>
2026-06-12 00:09:39 -0400

    Refactor autostart and general configuration

--- dot_config/hypr/conf/general.lua
+++ dot_config/hypr/conf/general.lua
@@ -1,194 +1,159 @@
 -- MONITOR CONFIG
 hl.monitor({
-    output = "eDP-1",
-    mode = "preferred@144Hz",
-    position = "0x0",
-    scale = 1,
+	output = "eDP-1",
+	mode = "preferred@144Hz",
+	position = "0x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output = "DP-1",
-    mode = "2560x1440@165Hz",
-    position = "1920x0",
-    scale = 1,
+	output = "DP-1",
+	mode = "2560x1440@165Hz",
+	position = "1920x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "1920x1080@144Hz",
-    position = "4480x0",
-    scale = 1,
+	output = "HDMI-A-1",
+	mode = "1920x1080@144Hz",
+	position = "4480x0",
+	scale = 1,
 })
 
 hl.gesture({
-    fingers = 3,
-    direction = "horizontal",
-    action = "workspace",
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
 
 hl.config({
-    general = {
-        gaps_in = 0,
-        gaps_out = 0,
+	general = {
+		gaps_in = 4,
+		gaps_out = 12,
 
-        border_size = 1,
+		border_size = 2,
 
-        col = {
-            active_border = "rgba(0DB7D455)",
-            inactive_border = "rgba(31313600)",
-        },
-        resize_on_border = true,
+		col = {
+			active_border = { colors = { "rgb(9b8d7f)", "rgba(59595900)", "rgb(aaaaaa)" }, angle = 45 },
+			inactive_border = "rgba(59595900)",
+		},
+		resize_on_border = true,
 
-        no_focus_fallback = true,
-        allow_tearing = true,
-    },
-    decoration = {
-        rounding_power = 0,
-        rounding = 0,
+		no_focus_fallback = true,
+		allow_tearing = true,
+	},
+	decoration = {
+		rounding_power = 0,
+		rounding = 0,
 
-        active_opacity = 1.0,
-        inactive_opacity = 0.9,
-        blur = {
-            enabled = true,
-            xray = true,
-            size = 10,
-            passes = 3,
-            vibrancy = 0.1696,
-            popups = false,
-            popups_ignorealpha = 0.6,
-            input_methods = true,
-            input_methods_ignorealpha = 0.8,
-        },
-        shadow = {
-            enabled = false,
-            range = 20,
-            offset = { 0, 2 },
-            render_power = 10,
-            color = "rgba(00000020)",
-        },
-        -- Dim
-        dim_inactive = true,
-        dim_strength = 0.1,
-        dim_special = 0.2,
-    },
-    animations = {
-        enabled = true,
-    },
-    dwindle = {
-        preserve_split = true,
-        smart_split = false,
-        smart_resizing = false,
-    },
+		active_opacity = 1.0,
+		inactive_opacity = 0.9,
+		blur = {
+			enabled = true,
+			xray = true,
+			size = 10,
+			passes = 3,
+			vibrancy = 0.1696,
+			popups = false,
+			popups_ignorealpha = 0.6,
+			input_methods = true,
+			input_methods_ignorealpha = 0.8,
+		},
+		shadow = {
+			enabled = true,
+			range = 10,
+			offset = { 0, 2 },
+			render_power = 5,
+			color = "rgb(9b8d7f)",
+			color_inactive = "rgba(242424aa)",
+		},
+		-- Dim
+		dim_inactive = true,
+		dim_strength = 0.1,
+		dim_special = 0.2,
+	},
+	animations = {
+		enabled = true,
+	},
+	dwindle = {
+		preserve_split = true,
+		smart_split = false,
+		smart_resizing = false,
+	},
 })
 -- Curves
-hl.curve("expressiveFastSpatial", {
-    type = "bezier",
-    points = { { 0.42, 1.67 }, { 0.21, 0.90 } },
-})
-hl.curve("expressiveSlowSpatial", {
-    type = "bezier",
-    points = { { 0.39, 1.29 }, { 0.35, 0.98 } },
-})
-hl.curve("expressiveDefaultSpatial", {
-    type = "bezier",
-    points = { { 0.38, 1.21 }, { 0.22, 1.00 } },
-})
-hl.curve("emphasizedDecel", {
-    type = "bezier",
-    points = { { 0.05, 0.7 }, { 0.1, 1 } },
-})
-hl.curve("emphasizedAccel", {
-    type = "bezier",
-    points = { { 0.3, 0 }, { 0.8, 0.15 } },
-})
-hl.curve("standardDecel", {
-    type = "bezier",
-    points = { { 0, 0 }, { 0, 1 } },
-})
-hl.curve("menu_decel", {
-    type = "bezier",
-    points = { { 0.1, 1 }, { 0, 1 } },
-})
-hl.curve("menu_accel", {
-    type = "bezier",
-    points = { { 0.52, 0.03 }, { 0.72, 0.08 } },
-})
-hl.curve("stall", {
-    type = "bezier",
-    points = { { 1, -0.1 }, { 0.7, 0.85 } },
-}) -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
 hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
-hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
-hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
+hl.curve("md3_standard", { type = "bezier", points = { { 0.2, 0 }, { 0, 1 } } })
+hl.curve("md3_decel", { type = "bezier", points = { { 0.05, 0.7 }, { 0.1, 1 } } })
+hl.curve("md3_accel", { type = "bezier", points = { { 0.3, 0 }, { 0.8, 0.15 } } })
+hl.curve("overshot", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.1 } } })
+hl.curve("crazyshot", { type = "bezier", points = { { 0.1, 1.5 }, { 0.76, 0.92 } } })
+hl.curve("hyprnostretch", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.0 } } })
+hl.curve("menu_decel", { type = "bezier", points = { { 0.1, 1 }, { 0, 1 } } })
+hl.curve("menu_accel", { type = "bezier", points = { { 0.38, 0.04 }, { 1, 0.07 } } })
+hl.curve("easeInOutCirc", { type = "bezier", points = { { 0.85, 0 }, { 0.15, 1 } } })
+hl.curve("easeOutCirc", { type = "bezier", points = { { 0, 0.55 }, { 0.45, 1 } } })
+hl.curve("easeOutExpo", { type = "bezier", points = { { 0.16, 1 }, { 0.3, 1 } } })
+hl.curve("softAcDecel", { type = "bezier", points = { { 0.26, 0.26 }, { 0.15, 1 } } })
+hl.curve("md2", { type = "bezier", points = { { 0.4, 0 }, { 0.2, 1 } } })
 
--- Default springs
-hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-
-hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
-hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
-hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
-hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
+-- Animation configs
+hl.animation({ leaf = "windows", enabled = true, speed = 3, bezier = "md3_decel", style = "popin 60%" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 3, bezier = "md3_decel", style = "popin 60%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, bezier = "md3_accel", style = "popin 60%" })
+hl.animation({ leaf = "border", enabled = true, speed = 10, bezier = "default" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3, bezier = "md3_decel" })
+hl.animation({ leaf = "layersIn", enabled = true, speed = 2.7, bezier = "menu_decel", style = "slide" })
+hl.animation({ leaf = "layersOut", enabled = true, speed = 1.6, bezier = "menu_accel" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 2, bezier = "menu_decel" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 4.5, bezier = "menu_accel" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 4.5, bezier = "md3_decel", style = "slide" })
+hl.animation({ leaf = "specialWorkspace", enabled = true, speed = 3, bezier = "md3_decel", style = "slidevert" })
 
 hl.config({
-    input = {
-        kb_layout = "us",
-        repeat_delay = 250,
-        repeat_rate = 35,
+	input = {
+		kb_layout = "us",
+		repeat_delay = 250,
+		repeat_rate = 35,
 
-        follow_mouse = 1,
-        off_window_axis_events = 2,
+		follow_mouse = 1,
+		off_window_axis_events = 2,
 
-        touchpad = {
-            natural_scroll = true,
-            disable_while_typing = true,
-            clickfinger_behavior = true,
-            scroll_factor = 0.7,
-        },
-    },
+		touchpad = {
+			natural_scroll = true,
+			disable_while_typing = true,
+			clickfinger_behavior = true,
+			scroll_factor = 0.7,
+		},
+	},
 
-    misc = {
-        disable_hyprland_logo = true,
-        disable_splash_rendering = true,
-        vrr = 0,
-        mouse_move_enables_dpms = true,
-        key_press_enables_dpms = true,
-        enable_swallow = false,
-        swallow_regex = "(foot|kitty|allacritty|Alacritty)",
-        on_focus_under_fullscreen = 2,
-        allow_session_lock_restore = true,
-        session_lock_xray = true,
-        initial_workspace_tracking = false,
-        focus_on_activate = true,
-    },
+	misc = {
+		disable_hyprland_logo = true,
+		disable_splash_rendering = true,
+		vrr = 3,
+		mouse_move_enables_dpms = true,
+		key_press_enables_dpms = true,
+		enable_swallow = true,
+		swallow_regex = "(ghostty|kitty)",
+		on_focus_under_fullscreen = 2,
+		allow_session_lock_restore = true,
+		initial_workspace_tracking = false,
+		focus_on_activate = true,
+	},
 
-    binds = {
-        scroll_event_delay = 0,
-        hide_special_on_workspace_change = true,
-    },
+	binds = {
+		scroll_event_delay = 0,
+		hide_special_on_workspace_change = true,
+	},
 
-    cursor = {
-        zoom_factor = 1,
-        zoom_rigid = false,
-        zoom_disable_aa = true,
-        hotspot_padding = 1,
-    },
+	cursor = {
+		zoom_factor = 1,
+		zoom_rigid = false,
+		zoom_disable_aa = true,
+		hotspot_padding = 1,
+	},
 
-    xwayland = {
-        force_zero_scaling = true,
-    },
+	xwayland = {
+		force_zero_scaling = true,
+	},
 })
